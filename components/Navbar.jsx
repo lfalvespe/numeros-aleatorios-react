81ad6c40d3937ebe724bcbb8996c320164484f06
Fernando Alves <lfalvespe@gmail.com>
2025-04-13 11:52:34 -0300

    navbar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,30 +2,33 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-
-        <nav>
-
+        <nav className="navbar d-flex justify-content-between">
+           
             <a
-                href=""
-            > <span class="link-icon">🎲 </span>Números aleatórios
-            </a>
-
-            <a
-                href="https://climagora.vercel.app/" target='_blank'
-            > <span class="link-icon">⛅ </span>Clima Agora
-            </a>
-
-            <a
-                href="https://contador-reactjs.vercel.app/" target="_blank"
-            > <span class="link-icon">🎰 </span>Contador Numérico
-            </a>
-
-            <a
+                className="text-decoration-none text-capitalize rounded"
                 href="https://consultaporcep.vercel.app/"
-                target="_blank"
-            > <span class="link-icon">📬 </span>Busca Cep
+                target='_blank'
+            > <span className="link-icon"><img src="location.png" alt="" className="nav-icon" /> </span>Busca Cep
             </a>
-
+            <a
+                className="text-decoration-none text-capitalize rounded"
+                href="https://climagora.vercel.app/"
+                target="_blank"
+            > <span className="link-icon"><img src="weather.png" alt="" className="nav-icon" /> </span>Clima Agora
+            </a>
+            <a
+                className="text-decoration-none text-capitalize rounded"
+                href="https://contador-reactjs.vercel.app/"
+                target="_blank"
+            > <span className="link-icon"><img src="sort-64.png" alt="" className="nav-icon" /> </span>Contador Numérico
+            </a>
+            <a
+                className="text-decoration-none text-capitalize rounded"
+                href="https://cronometro-reactjs.vercel.app/"
+                target="_blank"
+            > <span className="link-icon"><img src="chronometer-100.png" alt="" className="nav-icon" /> </span>Cronômetro
+            </a>
+            
         </nav>
     )
 }
